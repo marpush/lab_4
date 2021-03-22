5c28bfd608e065580f8839b9f3d021e8789e4487
Марьяна <@DESKTOP-8H8GHND>
2021-03-22 19:20:59 +0600

    Добавьте файлы проекта.

---
+++ lab_4/lab_4.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+#define BORDER 6
+
+double scan_double_number();
+void calculates_exponent(const double argument);
+int calculates_factorial(const double number);
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Данная программа вычисляет экспоненту в введенной точке\n";
+	cout << "Введите аргумент функции: ";
+	double x = scan_double_number();
+	calculates_exponent(x);
+}
+
+double scan_double_number()
+{
+	while (true) {
+		double number = 0;
+		cin >> number;
+
+		if (cin.fail()) {
+			cout << "Введенный символ не число.\n";
+			cin.clear();
+			std::cin.ignore(32767, '\n');
+		}
+		else {
+				return number;
+		}
+	}
+}
+
+void calculates_exponent(const double argument)
+{
+	double exp = 0;
+	for (int index = 0; index < BORDER; index++) {
+		exp = pow(argument, index) / calculates_factorial(index);
+	}
+	cout << "Значение экспоненты равно: " << exp;
+}
+
+int calculates_factorial(const double number) {
+	int factorial = 1, index;
+	for (index = 1; index <= number; index++) {
+		factorial = index * factorial;
+	}
+	return factorial;
+}
